978fe31564c061707cc8c3dfa44c7c1a55e45faa
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-12-19 23:53:35 -0600

    initial new changes

---
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,3 @@
+export const fault FirstApp = () => {
+  return <h1>First App Leo</h1>;
+};
--- 03-counter-app-vite/src/HelloWorldApp.jsx
+++ 03-counter-app-vite/src/HelloWorldApp.jsx
@@ -3,8 +3,10 @@
 //   }
 
 //   export default App;
-export const HelloWorldApp = () => {
-  return (
-    <h1>Hello World on React</h1>
-  )
-}
+ export const HelloWorldApp = () => {
+   return (
+     <h1>Hello World on React</h1>
+   )
+ }
+
+//export const HelloWorldApp = () => <h1>Hello World on React</h1>;
